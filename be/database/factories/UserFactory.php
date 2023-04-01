8dc8c4920531b13305860272dd15b8b74c48b545
Tran Duc Thanh <thanh.td@vinayamato.com>
2023-04-01 23:28:45 +0700

    fix production

--- be/database/factories/UserFactory.php
+++ be/database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
             'email' => $this->faker->safeEmail(),
             'account' => $this->faker->unique()->regexify('/^[A-Za-z0-9]{8,16}$/'),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2a$12$gJvbN4gaHLBXhQXWDeoHM.uoLWcrkvEKHOUeXrgSziQVMCl62K4Ju', // 123123123
             'remember_token' => Str::random(10),
         ];
     }
