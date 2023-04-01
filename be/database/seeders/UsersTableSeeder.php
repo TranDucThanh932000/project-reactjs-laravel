fa048e9b81954b0576697ec2dec57af695cdfc44
Tran Duc Thanh <thanh.td@vinayamato.com>
2023-04-01 23:43:36 +0700

    update seeder user

--- be/database/seeders/UsersTableSeeder.php
+++ be/database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'Trần Đức Thành',
+            'email' => 'tranducthanh932000@gmail.com',
+            'account' => 'thanhgpt2k',
+            'email_verified_at' => now(),
+            'password' => '$2a$12$gJvbN4gaHLBXhQXWDeoHM.uoLWcrkvEKHOUeXrgSziQVMCl62K4Ju', // 123123123
+            'remember_token' => Str::random(10),
+        ]);
         User::factory()->count(30)->create();
     }
 }
