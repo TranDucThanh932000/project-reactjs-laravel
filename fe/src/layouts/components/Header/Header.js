b5be2130be7a9cba7d0bba2fc3631f1ed6cde529
Tran Duc Thanh <thanh.td@vinayamato.com>
2023-05-15 08:36:01 +0700

    test input english on mobile

--- fe/src/layouts/components/Header/Header.js
+++ fe/src/layouts/components/Header/Header.js
@@ -479,7 +479,7 @@ const Header = (props) => {
               inputProps={{ "aria-label": "search" }}
             />
           </Search>
-            <input lang="ja-Hrkt" inputmode="kana"/>
+          <input lang="en"/>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton>
